39c58ad96ce17442f55910617d63df828642ae4e
varun01patel <varun01patel@gmail.com>
2024-04-08 14:03:47 +0530

    signup form is sending data to appwrite

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -15,10 +15,11 @@ import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/Validation";
 import Loader from "@/components/Shared/Loader";
 import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 
 const SignupForm = () => {
-    const isLoading = true;
+    const isLoading = false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -31,10 +32,10 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+   async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    //create a user 
+    const newUser = await createUserAccount(values);
+    console.log(newUser);
   }
   return (
     <Form {...form}>
